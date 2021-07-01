77bc8ef5ba9e566c8157eaff050fab7066ccd063
WindyArdolipikDenny <windyardolipikdenny@gmail.com>
2021-07-01 14:58:55 +0700

    update tampilan menggunakan parallax

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// navbar
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav ul");
 
@@ -7,7 +8,7 @@ menuToggle.addEventListener("click", function () {
 nav.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
-
+// contact
 const scriptURL = "https://script.google.com/macros/s/AKfycbyUV8VNjAj4gYOIcOW4PUyg-qL3P5vzsw1l9GlVXSVvU2XQHwhwZMFsPjdzBwOKmzCT/exec";
 const form = document.forms["my-portfolio-form"];
 const btnKirim = document.querySelector(".btn-kirim");
@@ -22,4 +23,38 @@ form.addEventListener("submit", (e) => {
       console.log("Success!", response);
     })
     .catch((error) => console.error("Error!", error.message));
+});
+// parallax load(dibuka/direfresh)
+// Abaout
+$(window).on("load", function () {
+  $(".pKiri").addClass("pMuncul");
+  $(".pTengah").addClass("pMuncul");
+  $(".pKanan").addClass("pMuncul");
+});
+
+// parallax Scroll
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  // header
+  $(".hero img").css({
+    transform: "translate(0px, " + wScroll / 10 + "%)",
+  });
+  $(".name").css({
+    transform: "translate(0px, " + wScroll / 18 + "%)",
+  });
+  $(".status").css({
+    transform: "translate(0px, " + wScroll / 20 + "%)",
+  });
+  $(".menu").css({
+    transform: "translate(0px, " + wScroll / 20 + "%)",
+  });
+
+  // galery
+  if (wScroll > $(".galery").offset().top - 200) {
+    $(".galery .thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".galery .thumbnail").eq(i).addClass("muncul");
+      }, 300 * (i + 1));
+    });
+  }
 });
